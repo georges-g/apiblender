294239a9c6ea8e9ef8e853ad599afd053a0febb5
Pierre Senellart <pierre@senellart.com>
2013-04-16 09:35:43 +0800

    New distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(  name='apiblender',
-        version='0.1',
+        version='0.2',
         description='API Blender allows you to easily interact with many APIs',
         author='Georges Gouriten',
         author_email='georges.gouriten@gmail.com',
